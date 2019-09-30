c54a433f11d9de79772b3da3faf7212c0ff50bb0
Jeremy Willhelm <46633603+CodeslayerJay@users.noreply.github.com>
2019-09-30 14:13:49 -0400

    Add project files.

---
+++ ProjectName.Infrastructure/Identity/IdentityDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectName.Infrastructure.Identity
+{
+    public class IdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        //private readonly string _connString = "Server=(localdb)\\MSSQLLocalDB_ADV;Database=;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+        public IdentityDbContext(DbContextOptions<IdentityDbContext> options) : base(options)
+        { }
+
+        //public IdentityDbContext()
+        //{ }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    if (!optionsBuilder.IsConfigured)
+        //    {
+
+        //        optionsBuilder.UseSqlServer(_connString);
+        //    }
+        //}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
